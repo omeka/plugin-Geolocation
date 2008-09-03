70573235e76fffe86f19953b6ffbae02f4a878a7
Jim Safley <jimsafley@gmail.com>
2008-09-03 18:27:27 +0000

    Integrates the Geolocation config form markup into plugin.php.

--- plugin.php
+++ plugin.php
@@ -81,7 +81,19 @@ function geo_add_routes($router)
 
 function geo_form()
 {
-    include 'form.php';
+?>
+<label for="default_latitude">Default Latitude:</label>
+<input type="text" name="default_latitude" size="8" value="<?php echo get_option('geo_default_latitude'); ?>" id="default_latitude" />
+
+<label for="default_longitude">Default Longitude:</label>
+<input type="text" name="default_longitude" size="8" value="<?php echo get_option('geo_default_longitude'); ?>" id="default_longitude" />
+
+<label for="default_zoomlevel">Default Zoom Level:</label>
+<input type="text" name="default_zoomlevel" size="3" value="<?php echo get_option('geo_default_zoom_level'); ?>" id="default_zoomlevel" />
+
+<label for="map_key">Google Maps API Key:</label>
+<input type="text" name="map_key" size="90" value="<?php echo get_option('geo_gmaps_key'); ?>" id="map_key" />
+<?php
 }
 
 function geo_config()
