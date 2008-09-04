931587f93d52282c7001fe3e76393a5a4b4fc04b
Jim Safley <jimsafley@gmail.com>
2008-09-04 17:39:56 +0000

    Updates the Geolocation KML file to the new API.

--- views/shared/map/browse.kml.php
+++ views/shared/map/browse.kml.php
@@ -2,7 +2,7 @@
 <kml xmlns="http://earth.google.com/kml/2.0">
     <Document>
         <name>Omeka Items KML</name>
-        <?php //Here is the styling for the balloon that appears on the map ?>    
+        <?php /* Here is the styling for the balloon that appears on the map */ ?>
         <Style id="item-info-balloon">
             <BalloonStyle>
                 <text><![CDATA[
@@ -10,34 +10,45 @@
                         <div class="title">$[name]</div>
                         <div class="description">$[Snippet]</div>
                         <div class="body">$[description]</div>
-                    </div>]]>
-                </text>
+                    </div>
+                ]]></text>
             </BalloonStyle>
         </Style>
-    <?php foreach( $items as $item ): ?>
-        <?php $location = $locations[$item->id]; ?>
-       <Placemark>
-            <name><![CDATA[<?php if($item->title) echo h($item->title); else echo '[Untitled]';?>]]></name>
-            <Snippet maxLines="2"><![CDATA[<?php   
-                    if($item->description):
-                        echo snippet(htmlentities($item->description), 0, 150); 
-                    else:
-                        echo 'No description was given.';
-                    endif; ?>]]></Snippet>    
+        <?php
+        while(loop_items()):
+        $item = get_current_item();
+        $location = $locations[$item->id];
+        ?>
+        <Placemark>
+            <name><![CDATA[<?php
+            if ($title = item('Title', array('element_set' => 'Dublin Core'))):
+                echo h($title[0]);
+            else:
+                echo '[Untitled]';
+            endif;
+            ?>]]></name>
+            <Snippet maxLines="2"><![CDATA[<?php
+            if ($description = item('Description', array('element_set' => 'Dublin Core'))):
+                echo snippet(htmlentities($description[0]), 0, 150); 
+            else:
+                echo 'No description was given.';
+            endif;
+            ?>]]></Snippet>    
             <description><![CDATA[<?php 
-            /* @since 3/26/08: movies do not display properly on the map in IE6, so can't use display_files()  
-            Description field contains the HTML for displaying the first file (if possible)
-            echo display_files($item->Files[0]); */
+            // @since 3/26/08: movies do not display properly on the map in IE6, 
+            // so can't use display_files(). Description field contains the HTML 
+            // for displaying the first file (if possible)
+            //echo display_files($item->Files[0]);
             echo thumbnail($item->Files[0]);
-            echo link_to_item($item, 'show', 'View Item');
+            echo link_to_item('show', 'Item');
             ?>]]></description>
             <Point>
-             <coordinates><?php echo $location['longitude']; ?>,<?php echo $location['latitude']; ?></coordinates>
+                <coordinates><?php echo $location['longitude']; ?>,<?php echo $location['latitude']; ?></coordinates>
             </Point>
-            <?php if ( $location['address'] ): ?>
-                <address><?php echo htmlentities($location['address']); ?></address>
+            <?php if ($location['address']): ?>
+            <address><?php echo htmlentities($location['address']); ?></address>
             <?php endif; ?>
         </Placemark>
-    <?php endforeach; ?>
+        <?php endwhile; ?>
     </Document>
 </kml>
