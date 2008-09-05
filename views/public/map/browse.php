7879e04db14974172e3652e765fc20d4bef94e92
Kristopher Kelly <kristopherbkelly@gmail.com>
2008-09-05 16:18:51 +0000

    Fixes markup on public map browse.

--- views/public/map/browse.php
+++ views/public/map/browse.php
@@ -1,29 +1,30 @@
 <?php head(); ?>
-<?php geolocation_scripts(); ?>
 
-<style type="text/css" media="screen">
-	#map_browse {
-		width: 500px;
-		height: 500px;
-	}
-	
-	#map-links li {overflow:hidden; border-bottom:1px dotted #ccc;}
-	#map-links li a {float:right; width:50%; text-decoration:none; }
-</style>
+<?php geolocation_scripts(); ?>
 
 <div id="primary">
 
-<h1>Browse Items on the Map</h1>
-<div class="pagination"><?php echo map_pagination(); ?></div>
+<style type="text/css" media="screen">
+#map_browse { width: 500px; height: 500px;}
+#map-links li {overflow:hidden; border-bottom:1px dotted #ccc;}
+#map-links li a {float:right; width:50%; text-decoration:none; }
+</style>
 
-<?php 
-	 google_map('map_browse', array('uri'=>uri('map/browse'), 'list'=>'map-links'));
-?>
+<h1>Browse Items on the Map (<?php echo $totalItems; ?> items total)</h1>
 
-<h2>Find An Item on the Map</h2>
+<div class="pagination">
+    <?php echo pagination(); ?>
+</div><!-- end pagination -->
 
-<div id="map-links"></div>
+<div id="map_block">
+    <?php google_map('map_browse', array('uri'=>uri('geolocation/map/browse'), 'list'=>'map-links'));?>
+</div><!-- end map_block -->
 
-</div>
+<div id="link_block">
+    <h2>Find An Item on the Map</h2>
+    <div id="map-links"></div><!-- Used by JavaScript -->
+</div><!-- end link_block -->
+
+</div><!-- end primary -->
 
 <?php foot(); ?>
