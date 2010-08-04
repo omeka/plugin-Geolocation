5a336f14917f400e6e816ecbcbde6c6ec43fe690
John Flatness <john@zerocrates.org>
2010-08-04 13:35:44 +0000

    Removes obsolete test bootstrap.

--- tests/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('GEOLOCATION_DIR', dirname(dirname(__FILE__)));
-$baseBootstrap = dirname(dirname(dirname(dirname(__FILE__)))) . '/application/tests/bootstrap.php';
-require_once $baseBootstrap;
-require_once 'Geolocation_TestCase.php';
-require_once 'Geolocation_ViewTestCase.php';
