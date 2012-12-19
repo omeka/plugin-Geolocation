feff3390c45a72871ea97f25ab5877caf4a786d3
Patrick Murray-John <patrickmjchnm@gmail.com>
2012-12-19 14:09:51 -0500

    more work on #5

--- models/Table/Location.php
+++ models/Table/Location.php
@@ -69,7 +69,7 @@ class Table_Location extends Omeka_Db_Table
         $itemTable = $this->_db->getTable('Item'); 
         $select = $itemTable->getSelectForFindBy($params, $limit, $page);
         $alias = $this->getTableAlias();
-        $select->join(array($alias, $this->getTableName()),                
+        $select->join(array($alias, $this->_db->Location),                
                 "items.id = $alias.item_id",
                 array());
         $this->applySearchFilters($select, $params);
