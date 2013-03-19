4eb9a28239b3f15a6d0b196f9e18f9029fe5ca87
Kim Nguyen <design@kimisgold.com>
2013-03-19 11:09:26 -0400

    Make browse views more responsive and pretty.

--- views/admin/map/browse.php
+++ views/admin/map/browse.php
@@ -4,17 +4,14 @@
     
     $css = "
             #map_browse {
-                width: 569px;
                 height: 436px;
-        		float:left;
-        		border:1px solid #ccc; border-width: 1px 1px 1px 0;
             }
-        	.balloon {width:400px !important; font-size:1.2em;}
-        	.balloon .title {font-weight:bold;margin-bottom:1.5em;} 
-        	.balloon .title, .balloon .description {float:left; width: 220px;margin-bottom:1.5em;}
-        	.balloon img {float:right;display:block;}
-        	.balloon .view-item {display:block; float:left; clear:left; font-weight:bold; text-decoration:none;}
-        	#map-links ul { margin: 0;  float:left; padding:18px 18px 18px 27px; border:1px solid #ccc; border-width: 1px 0 1px 1px;width: 191px; height: 400px; overflow-y:auto; list-style:square;}
+            .balloon {width:400px !important; font-size:1.2em;}
+            .balloon .title {font-weight:bold;margin-bottom:1.5em;} 
+            .balloon .title, .balloon .description {float:left; width: 220px;margin-bottom:1.5em;}
+            .balloon img {float:right;display:block;}
+            .balloon .view-item {display:block; float:left; clear:left; font-weight:bold; text-decoration:none;}
+            #map-links ul { margin: 0; overflow-y:auto; list-style:square;}
             #map-links a {
                 display:block;
             }
@@ -30,23 +27,23 @@
 <?php echo head(array('title'=>$title)); ?>
 
 <div id="geolocation-browse">
-	<div class="pagination">
-	    <?php echo pagination_links(); ?>
-	</div><!-- end pagination -->
+    <div class="pagination">
+        <?php echo pagination_links(); ?>
+    </div><!-- end pagination -->
+    
+    <div id="map_block">
+        <?php echo $this->googleMap('map_browse', array('loadKml'=>true, 'list'=>'map-links'));?>
+    </div><!-- end map_block -->
 
-	<div id="link_block">
-	    <h2>Find An Item on the Map</h2>
-	    <div id="map-links"></div><!-- Used by JavaScript -->
-	</div><!-- end link_block -->
+    <div id="link_block">
+        <div id="map-links"><h2>Find An Item on the Map</h2></div><!-- Used by JavaScript -->
+    </div><!-- end link_block -->
 
-	<div id="map_block">
-	    <?php echo $this->googleMap('map_browse', array('loadKml'=>true, 'list'=>'map-links'));?>
-	</div><!-- end map_block -->
 
-	<div id="search_block">
-	    <?php echo items_search_form(array('id'=>'search'), $_SERVER['REQUEST_URI']); ?>
-	</div><!-- end search_block -->
+    <div id="search_block">
+        <?php echo items_search_form(array('id'=>'search'), $_SERVER['REQUEST_URI']); ?>
+    </div><!-- end search_block -->
 </div>
 
 
-<?php foot(); ?>
+<?php echo foot(); ?>
--- views/public/map/browse.php
+++ views/public/map/browse.php
@@ -5,17 +5,13 @@ queue_js_file('map');
 
 $css = "
             #map_browse {
-                width: 569px;
                 height: 436px;
-        		float:left;
-        		border:1px solid #ccc; border-width: 1px 1px 1px 0;
             }
-        	.balloon {width:400px !important; font-size:1.2em;}
-        	.balloon .title {font-weight:bold;margin-bottom:1.5em;}
-        	.balloon .title, .balloon .description {float:left; width: 220px;margin-bottom:1.5em;}
-        	.balloon img {float:right;display:block;}
-        	.balloon .view-item {display:block; float:left; clear:left; font-weight:bold; text-decoration:none;}
-        	#map-links ul { margin: 0;  float:left; padding:18px 18px 18px 27px; border:1px solid #ccc; border-width: 1px 0 1px 1px;width: 191px; height: 400px; overflow-y:auto; list-style:square;}
+            .balloon {width:400px !important; font-size:1.2em;}
+            .balloon .title {font-weight:bold;margin-bottom:1.5em;}
+            .balloon .title, .balloon .description {float:left; width: 220px;margin-bottom:1.5em;}
+            .balloon img {float:right;display:block;}
+            .balloon .view-item {display:block; float:left; clear:left; font-weight:bold; text-decoration:none;}
             #map-links a {
                 display:block;
             }
@@ -26,7 +22,6 @@ queue_css_string($css);
 
 echo head(array('title' => __('Browse Map'),'bodyid'=>'map','bodyclass' => 'browse')); ?>
 
-<div id="primary">
 
 <h1>Browse Items on the Map (<?php echo $totalItems; ?> total)</h1>
 
@@ -38,12 +33,14 @@ echo head(array('title' => __('Browse Map'),'bodyid'=>'map','bodyclass' => 'brow
     <?php echo pagination_links(); ?>
 </div><!-- end pagination -->
 
-<div id="map-block">
+<div id="primary">
+
+<div id="map_block">
     <?php echo $this->googleMap('map_browse', array('loadKml'=>true, 'list'=>'map-links'));?>
 </div><!-- end map_block -->
 
 <div id="link_block">
-    <div id="map-links"></div><!-- Used by JavaScript -->
+    <div id="map-links"><h2>Find An Item on the Map</h2></div><!-- Used by JavaScript -->
 </div><!-- end link_block -->
 
 </div><!-- end primary -->
