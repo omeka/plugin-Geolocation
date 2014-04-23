4694a7d4cdf5ce9fac152867d29e08ff612f2f81
John Flatness <john@zerocrates.org>
2014-04-23 19:52:00 -0400

    Remove useless Location table proxies to Item.

--- controllers/MapController.php
+++ controllers/MapController.php
@@ -4,7 +4,7 @@ class Geolocation_MapController extends Omeka_Controller_AbstractActionControlle
 {
     public function init()
     {
-        $this->_helper->db->setDefaultModelName('Location');
+        $this->_helper->db->setDefaultModelName('Item');
     }
     
     public function browseAction()
@@ -13,16 +13,17 @@ class Geolocation_MapController extends Omeka_Controller_AbstractActionControlle
         $this->_setParam('use_map_per_page', true);
         
         $table = $this->_helper->db->getTable();
+        $locationTable = $this->_helper->db->getTable('Location');
         
         $params = $this->getAllParams();
         $currentPage = $this->getParam('page', 1);
         $limit = (int)get_option('geolocation_per_page');
         $params['only_map_items'] = true;
-        $items = $table->findItemsBy($params, $limit, $currentPage);
-        
+
+        $items = $table->findBy($params, $limit, $currentPage);
         $this->view->items = $items;
-        $this->view->locations = $table->findLocationByItem($items);
-        $this->view->totalItems = $table->countItemsBy($params);
+        $this->view->locations = $locationTable->findLocationByItem($items);
+        $this->view->totalItems = $table->count($params);
         
         $params = array('page'  => $currentPage,
                 'per_page'      => $limit,
