1e1b5dc29abb92a51930793a5c9363c4d63f77be
John Flatness <john@zerocrates.org>
2014-04-24 14:17:38 -0400

    Bring public view in line with current theming.

--- views/public/map/browse.php
+++ views/public/map/browse.php
@@ -1,24 +1,24 @@
 <?php 
 queue_css_file('geolocation-items-map');
 
-echo head(array('title' => __('Browse Map'),'bodyid'=>'map','bodyclass' => 'browse'));
+$title = __('Browse Items on the Map') . ' ' . __('(%s total)', $totalItems);
+echo head(array('title' => $title, 'bodyclass' => 'map browse'));
 ?>
 
-<h1><?php echo __('Browse Items on the Map');?> (<?php echo $totalItems; ?> <?php echo __('total');?>)</h1>
+<h1><?php echo $title; ?></h1>
 
 <nav class="items-nav navigation secondary-nav">
     <?php echo public_nav_items(); ?>
 </nav>
 
-<?php echo pagination_links(); ?>
-
-<div id="primary">
+<?php
+echo item_search_filters();
+echo pagination_links();
+?>
 
 <div id="geolocation-browse">
     <?php echo $this->googleMap('map_browse', array('list' => 'map-links', 'params' => $params)); ?>
     <div id="map-links"><h2><?php echo __('Find An Item on the Map'); ?></h2></div>
 </div>
-
-</div><!-- end primary -->
 
 <?php echo foot(); ?>
