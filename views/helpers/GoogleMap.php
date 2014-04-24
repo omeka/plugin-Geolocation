dfcc3a85789111b294fe0a6385adde3ee2416354
John Flatness <john@zerocrates.org>
2014-04-24 14:22:20 -0400

    Allow further browse overrides.

--- views/helpers/GoogleMap.php
+++ views/helpers/GoogleMap.php
@@ -2,16 +2,17 @@
 
 class Geolocation_View_Helper_GoogleMap extends Zend_View_Helper_Abstract
 {
-    public function googleMap($divId = 'map', $options = array())
+    public function googleMap($divId = 'map', $options = array(), $center = array())
     {
-        // Load this junk in from the plugin config
-        $center = array(
-                'latitude'=>  (double) get_option('geolocation_default_latitude'), 
-                'longitude'=> (double) get_option('geolocation_default_longitude'), 
-                'zoomLevel'=> (double) get_option('geolocation_default_zoom_level'));
-        
-        // The request parameters get put into the map options
-        if (!isset($options['params'])) {
+        if (!$center) {
+            $center = array(
+                'latitude'  => (double) get_option('geolocation_default_latitude'),
+                'longitude' => (double) get_option('geolocation_default_longitude'),
+                'zoomLevel' => (double) get_option('geolocation_default_zoom_level')
+            );
+        }
+
+        if (!array_key_exists('params', $options)) {
             $options['params'] = array();
         }
 
