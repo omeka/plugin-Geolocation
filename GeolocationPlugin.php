7a5be7e6c4999b334bb184718f7ecd850f11c34f
Adam Doan <doana@uoguelph.ca>
2015-09-14 09:38:00 -0400

    Add accessible markup option to configuration form.

--- GeolocationPlugin.php
+++ GeolocationPlugin.php
@@ -62,6 +62,7 @@ class GeolocationPlugin extends Omeka_Plugin_AbstractPlugin
         set_option('geolocation_add_map_to_contribution_form', '0');
         set_option('geolocation_default_radius', 10);
         set_option('geolocation_use_metric_distances', '0');
+        set_option('geolocation_accessible_markup', '0');
     }
 
     public function hookUninstall()
@@ -73,6 +74,7 @@ class GeolocationPlugin extends Omeka_Plugin_AbstractPlugin
         delete_option('geolocation_per_page');
         delete_option('geolocation_add_map_to_contribution_form');
         delete_option('geolocation_use_metric_distances');
+        delete_option('geolocation_accessible_markup');
 
         // This is for older versions of Geolocation, which used to store a Google Map API key.
         delete_option('geolocation_gmaps_key');
@@ -126,6 +128,7 @@ class GeolocationPlugin extends Omeka_Plugin_AbstractPlugin
         set_option('geolocation_use_metric_distances', $_POST['geolocation_use_metric_distances']);
         set_option('geolocation_map_type', $_POST['map_type']);
         set_option('geolocation_auto_fit_browse', $_POST['auto_fit_browse']);
+        set_option('geolocation_accessible_markup', $_POST['geolocation_accessible_markup']);
     }
 
     public function hookDefineAcl($args)
